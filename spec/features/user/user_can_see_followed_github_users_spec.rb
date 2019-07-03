c4fa1c519b39e7e178ac8e5227dfcedb5f4ea3a0
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-03 16:54:49 -0600

    Add UserShowFacade/GithubApiService#followed_users
    Co-authored-by: Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>

--- spec/features/user/user_can_see_followed_github_users_spec.rb
+++ spec/features/user/user_can_see_followed_github_users_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe "As a user on my dashboard page" do
   it "I see a list of github users I'm following" do
     # VCR.use_cassette('user_followings') do
+    WebMock.allow_net_connect!
+    VCR.turn_off!
       user = create(:user)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -11,9 +13,9 @@ describe "As a user on my dashboard page" do
 
       within("#github_followed_users") do
         expect(page).to have_content("Followed Users")
-        expect(page.all(".followed_user_list").count).to eq(999)
+        expect(page.all(".followed_user_list").count).to eq(5)
         within(first(".followed_user_list")) do
-          expect(page).to have_link("somebody")
+          expect(page).to have_link("Mycobee")
         end
       end
     # end
