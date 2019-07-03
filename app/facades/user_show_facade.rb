26cc9d6e1d7217b76dff8f10bdc78e0864777f82
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-03 16:14:33 -0600

    Adds functionality for github user can see follower.
    Co-authored-by: Alexandra Chakeres <alexchakeres@gmail.com>

--- app/facades/user_show_facade.rb
+++ app/facades/user_show_facade.rb
@@ -5,4 +5,11 @@ class UserShowFacade
       Repo.new(repo_data)
     end.first(5)
   end
+
+  def followers
+    followers_hash = GithubApiService.new.user_followers
+    followers_hash.map do |follower_data|
+      GithubUser.new(follower_data)
+    end
+  end
 end
--- spec/features/user/user_can_see_github_followers_spec.rb
+++ spec/features/user/user_can_see_github_followers_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe "As a user on my dashboard page" do
   it 'I see a list of my github followers' do
-    VCR.use_cassette('user_followers') do
+    #VCR.use_cassette('user_followers') do
+      WebMock.allow_net_connect!
+      VCR.turn_off!
       user = create(:user)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -15,7 +17,7 @@ describe "As a user on my dashboard page" do
         within(first(".follower_list")) do
           expect(page).to have_link("kylecornelissen")
         end
-      end
+    #  end
     end
   end
 end
