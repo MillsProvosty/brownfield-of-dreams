9959169647428f0d3a4fd9577ab6bdbf4f772235
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-03 10:40:00 -0600

    Add test of Repo attributes
    Co-authored-by: Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Repo, type: :model do
+  before(:each) do
+    @repo_data = {name: "little shop", url: "little shop URL"}
+    @repo = Repo.new(@repo_data)
+  end
+
   it 'exists' do
-    repo_data = {name: "little shop", url: "little shop URL"}
-    expect(Repo.new(repo_data)).to be_a(Repo)
+    expect(@repo).to be_a(Repo)
+  end
+
+  it 'has attributes' do
+    expect(@repo.name).to eq(@repo_data[:name])
+    expect(@repo.url).to eq(@repo_data[:url])
   end
 end
