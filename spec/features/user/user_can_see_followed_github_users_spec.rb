3d87a71c60a7225f3f4267726217886f6f49ab4a
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-03 16:45:22 -0600

    Add feature test for list of github users followed
    Co-authored-by: Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>

---
+++ spec/features/user/user_can_see_followed_github_users_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "As a user on my dashboard page" do
+  it "I see a list of github users I'm following" do
+    # VCR.use_cassette('user_followings') do
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+
+      within("#github_followed_users") do
+        expect(page).to have_content("Followed Users")
+        expect(page.all(".followed_user_list").count).to eq(999)
+        within(first(".followed_user_list")) do
+          expect(page).to have_link("somebody")
+        end
+      end
+    # end
+  end
+end
