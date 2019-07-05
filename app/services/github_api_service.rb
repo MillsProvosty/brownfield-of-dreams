e20c97be631b6bf390e92509022a14fcaa3c953a
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-04 18:58:39 -0600

    Update GithubApiService to use user's access_token param instead of Authorization header

--- app/services/github_api_service.rb
+++ app/services/github_api_service.rb
@@ -1,4 +1,8 @@
 class GithubApiService
+  def initialize(user_id)
+    @user_id = user_id
+  end
+
   def user_repos
     fetch_data('/user/repos')
   end
@@ -15,14 +19,17 @@ class GithubApiService
 
   def conn
     Faraday.new(url:"https://api.github.com") do |f|
-      f.headers['Authorization'] = "token #{ENV["GITHUB_API_KEY"]}"
+      # TO DO: delete if possible... will likely break lots of tests
+      # f.headers['Authorization'] = "token #{ENV["GITHUB_API_KEY"]}"
       f.adapter  Faraday.default_adapter
     end
   end
 
   def fetch_data(uri_path)
-    response = conn.get uri_path
+    response = conn.get do |req|
+      req.url uri_path
+      req.params["access_token"] = User.find(@user_id).github_token
+    end
     JSON.parse(response.body, symbolize_names: true)
   end
-
 end
