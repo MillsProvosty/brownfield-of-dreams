077e83b47a6eb1a99af20de2d82feb15a6aad1f0
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-07 20:04:49 -0600

    Adds test for when friending a not registered user
    Co-authored-by: Alexandra Chakeres <alexchakeres@gmail.com>

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -53,9 +53,8 @@ describe "As a user on my dashboard page" do
   end
 
   it "I get an error message when I try to add someone I'm already friends with" do
-    #VCR.use_cassette('github_already_friend', :record => :new_episodes) do
-    WebMock.allow_net_connect!
-    VCR.turn_off!
+    VCR.use_cassette('github_already_friend', :record => :new_episodes) do
+
       user1 = create(:user_with_github)
       user2 = create(:user_with_github, github_handle: "kylecornelissen", github_url: "https://github.com/kylecornelissen")
 
@@ -71,7 +70,28 @@ describe "As a user on my dashboard page" do
 
       expect(page).to have_content("#{user2.github_handle} is already your friend")
 
-    #end
+    end
+  end
+
+  it "I get an error message when I try to add someone who isn't a registered user" do
+    VCR.use_cassette('github_user_not_registered', :record => :new_episodes) do
+
+      user1 = create(:user_with_github)
+      user2 = create(:user_with_github, github_handle: "kylecornelissen", github_url: "https://github.com/kylecornelissen")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+
+      visit '/dashboard'
+
+      user2.destroy
+
+      within(first(".follower_list")) do
+        click_button "Add as Friend"
+      end
+
+      expect(page).to have_content("#{user2.github_handle} is not a registered user")
+
+    end
   end
 
 
