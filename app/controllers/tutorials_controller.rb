4d6e256e01a1305062e008d64fd53abf7eecb500
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-08 13:45:01 -0600

    Adds spec and test for only current users being able to view tutorials

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class TutorialsController < ApplicationController
+  before_action :require_user
+
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
+  end
+
+  private
+  def require_user
+    render file: "/public/404" unless current_user
   end
 end
