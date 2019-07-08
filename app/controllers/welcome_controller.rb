c21dbba2aee2a25438d73ee760943f7e2d4a2722
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-08 16:53:25 -0600

    Refactors the welcome controller to dry up code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,28 +1,19 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-
-  def user_paginator
-    if params[:tag]
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
-    else
-      @tutorials = Tutorial.paginate(page: params[:page], per_page: 5)
-    end
-  end
-
-  def public_paginator
-    if params[:tag]
-      @tutorials = Tutorial.public_videos.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
-    else
-      @tutorials = Tutorial.public_videos.paginate(page: params[:page], per_page: 5)
-    end
-  end
-
   def index
+    if params[:tag]
+        @tutorials = filter_tutorials.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+    else
+        @tutorials = filter_tutorials.paginate(page: params[:page], per_page: 5)
+    end
+  end
+
+  def filter_tutorials
     if current_user
-      user_paginator
-    elsif !current_user
-      public_paginator
+      Tutorials.all
+    else !current_user
+      Tutorials.private_tutorials
     end
   end
 end
