824dcc5ef131b5aa031b8eab67aa0fddc6ea41f4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-08 14:29:25 -0600

    Fix bug on tutorial show page (when there are no associated videos)

---
+++ spec/features/vistors/visitor_can_see_list_of_videos_for_a_tutorial_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'visitor visits tutorial show page' do
+  it 'sees a list of videos associated with that tutorial' do
+    tutorial = create(:tutorial)
+    video_1, video_2 = create_list(:video, 2, tutorial: tutorial)
+
+    visit "/tutorials/#{tutorial.id}"
+
+    within("#video-list") do
+      expect(page.all(".show-link").count).to eq(2)
+      expect(first(".show-link")).to have_content(video_1.title)
+    end
+  end
+
+  it 'does not error out when there are no videos for the tutorial' do
+    tutorial = create(:tutorial)
+
+    visit "/tutorials/#{tutorial.id}"
+
+    expect(page.all(".show-link").count).to eq(0)
+    expect(page).to have_content("This tutorial has no videos")
+  end
+end
