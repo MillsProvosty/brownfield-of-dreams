957aacba204369c8f350d1471da821ccf923cd26
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-07 18:20:41 -0600

    Adds github_user#can_be_friend_of in order to determine if user is already a friend.
    Co-authored-by: Alexandra Chakeres <alexchakeres@gmail.com>

--- app/models/github_user.rb
+++ app/models/github_user.rb
@@ -5,4 +5,12 @@ class GithubUser
     @handle = attributes[:login]
     @url = attributes[:html_url]
   end
-end 
+
+  def can_be_friend_of?(current_user)
+    friend = User.find_by(github_handle: @handle)
+    user_exists = friend != nil
+    friendship = Friendship.find_by(user_id: current_user.id, friend_id: friend.id)
+    already_friends = friendship != nil
+    user_exists && !already_friends
+  end
+end
