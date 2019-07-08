14b7c2ac53631f06bba5c8815d82a3b3615d57bb
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-08 14:44:11 -0600

    Adds tutorials marked classroom to spec

--- spec/features/vistors/visitor_must_log_in_to_see_tutorials_spec.rb
+++ spec/features/vistors/visitor_must_log_in_to_see_tutorials_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe 'visitor cannot see tutorials', :js do
-  it 'until they have logged in' do
+  it 'until they have logged in, and they are classroom content' do
     # VCR.use_cassette('visitor_no_tutorials', record: :new_episodes) do
       WebMock.allow_net_connect!
       VCR.turn_off!
@@ -11,10 +11,10 @@ describe 'visitor cannot see tutorials', :js do
       tutorial1 = create(:tutorial)
       tutorial2 = create(:tutorial)
 
-      video1 = create(:video, tutorial_id: tutorial1.id)
-      video2 = create(:video, tutorial_id: tutorial1.id)
-      video3 = create(:video, tutorial_id: tutorial2.id)
-      video4 = create(:video, tutorial_id: tutorial2.id)
+      video1 = create(:video, tutorial_id: tutorial1.id, classroom: true)
+      video2 = create(:video, tutorial_id: tutorial1.id, classroom: false)
+      video3 = create(:video, tutorial_id: tutorial2.id, classroom: true)
+      video4 = create(:video, tutorial_id: tutorial2.id, classroom: false)
 
       visit root_path
 
@@ -28,18 +28,18 @@ describe 'visitor cannot see tutorials', :js do
     # end
   end
 
-  it 'can see tutorials once logged in ' do
+  it 'can only see tutorials once logged in and the content is marked classroom. ' do
     # VCR.use_cassette('visitor_see_tutorials', record: :new_episodes) do
       WebMock.allow_net_connect!
       VCR.turn_off!
-      
+
       tutorial1 = create(:tutorial)
       tutorial2 = create(:tutorial)
 
-      video1 = create(:video, tutorial_id: tutorial1.id)
-      video2 = create(:video, tutorial_id: tutorial1.id)
-      video3 = create(:video, tutorial_id: tutorial2.id)
-      video4 = create(:video, tutorial_id: tutorial2.id)
+      video1 = create(:video, tutorial_id: tutorial1.id, classroom: true)
+      video2 = create(:video, tutorial_id: tutorial1.id, classroom: false)
+      video3 = create(:video, tutorial_id: tutorial2.id, classroom: true)
+      video4 = create(:video, tutorial_id: tutorial2.id, classroom: false)
 
       user = create(:user)
 
@@ -65,6 +65,27 @@ describe 'visitor cannot see tutorials', :js do
         expect(page).to have_css('.tutorial-description')
         expect(page).to have_content(tutorial1.title)
         expect(page).to have_content(tutorial1.description)
+      end
+
+      within(page.all('.tutorials')[1]) do
+        expect(page).to_not have_css('.tutorial')
+        expect(page).to_not have_css('.tutorial-description')
+        expect(page).to_not have_content(tutorial1.title)
+        expect(page).to_not have_content(tutorial1.description)
+      end
+
+      within(page.all('.tutorials')[2]) do
+        expect(page).to have_css('.tutorial')
+        expect(page).to have_css('.tutorial-description')
+        expect(page).to have_content(tutorial1.title)
+        expect(page).to have_content(tutorial1.description)
+      end
+
+      within(page.all('.tutorials')[3]) do
+        expect(page).to_not have_css('.tutorial')
+        expect(page).to_not have_css('.tutorial-description')
+        expect(page).to_not have_content(tutorial1.title)
+        expect(page).to_not have_content(tutorial1.description)
       end
     # end
   end
