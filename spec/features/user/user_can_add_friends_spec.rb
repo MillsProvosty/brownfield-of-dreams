9acfc7899bb7e97634d3e66311d0c4eb3f50b794
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-07 20:02:57 -0600

    Adds test for trying to add friend thats already friend
    Co-authored-by: Alexandra Chakeres <alexchakeres@gmail.com>

--- spec/features/user/user_can_add_friends_spec.rb
+++ spec/features/user/user_can_add_friends_spec.rb
@@ -1,12 +1,8 @@
 require 'rails_helper'
 
 describe "As a user on my dashboard page" do
-  it "I see a button to login to github" do
-    #VCR.use_cassette('github_auth', :record => :new_episodes) do
-      WebMock.allow_net_connect!
-      VCR.turn_off!
-
-
+  it "I see a button to add friends next to followers and followed users" do
+    VCR.use_cassette('github_add_friend', :record => :new_episodes) do
       user1 = create(:user_with_github)
       user2 = create(:user_with_github, github_handle: "kylecornelissen", github_url: "https://github.com/kylecornelissen")
       user3 = create(:user_with_github, github_handle: "m-mrcr", github_url: "https://github.com/m-mrcr")
@@ -18,6 +14,8 @@ describe "As a user on my dashboard page" do
       within(first(".follower_list")) do
         click_button "Add as Friend"
       end
+
+      expect(page).to have_content("#{user2.github_handle} added as a friend")
 
       within(page.all(".follower_list")[1]) do
         expect(page).to_not have_button("Add as Friend")
@@ -38,6 +36,8 @@ describe "As a user on my dashboard page" do
         click_button "Add as Friend"
       end
 
+      expect(page).to have_content("#{user3.github_handle} added as a friend")
+
       within(page.all(".friend_list")[1]) do
         expect(page).to have_link("m-mrcr")
       end
@@ -49,7 +49,30 @@ describe "As a user on my dashboard page" do
       within(first(".followed_user_list")) do
         expect(page).to_not have_button("Add as Friend")
       end
+    end
+  end
+
+  it "I get an error message when I try to add someone I'm already friends with" do
+    #VCR.use_cassette('github_already_friend', :record => :new_episodes) do
+    WebMock.allow_net_connect!
+    VCR.turn_off!
+      user1 = create(:user_with_github)
+      user2 = create(:user_with_github, github_handle: "kylecornelissen", github_url: "https://github.com/kylecornelissen")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+
+      visit '/dashboard'
+
+      user1.friendships.create(friend: user2)
+
+      within(first(".follower_list")) do
+        click_button "Add as Friend"
+      end
+
+      expect(page).to have_content("#{user2.github_handle} is already your friend")
 
     #end
   end
+
+
 end
