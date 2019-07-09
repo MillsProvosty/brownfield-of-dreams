96d9996f779035194c9a54f8ec83ba35a6e8a1c0
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-09 09:56:15 -0600

    Further changes to test and show

--- spec/features/user/user_can_see_list_of_bookmarks.rb
+++ spec/features/user/user_can_see_list_of_bookmarks.rb
@@ -1,42 +1,62 @@
 require 'rails_helper'
 
 describe 'As a user on my dashboard page' do
-  it 'I see a list of my github repos' do
+  it 'I see a list of my bookmarked segments' do
     # VCR.use_cassette('see_ordered_bookmarks', record: :new_episodes) do
       WebMock.allow_net_connect!
       VCR.turn_off!
 
       user = create(:user)
-      tutorial1 = create(:tutorial)
-      tutorial2 = create(:tutorial)
-      tutorial3 = create(:tutorial)
+      tutorial1 = create(:tutorial, title: 'How to Tie Your Shoes')
+      tutorial2 = create(:tutorial, title: 'Prework - Environment Setup')
+      tutorial3 = create(:tutorial, title: 'Mod 3')
 
-      video1 = create(:video, tutorial_id: tutorial1.id)
-      video2 = create(:video, tutorial_id: tutorial2.id)
-      video3 = create(:video, tutorial_id: tutorial3.id)
+      video1 = create(:video, tutorial_id: tutorial1.id, title: 'The Bunny Ears Technique')
+      video2 = create(:video, tutorial_id: tutorial2.id, title: 'Bundle Install')
+      video3 = create(:video, tutorial_id: tutorial3.id, title: 'Rails Integration Testing')
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      # As a logged in user
+      visit tutorial_path(tutorial1)
 
+      expect do
+        click_on 'Bookmark'
+      end.to change { UserVideo.count }.by(1)
+
+      visit tutorial_path(tutorial2)
+
+      expect do
+        click_on 'Bookmark'
+      end.to change { UserVideo.count }.by(1)
+
+      visit tutorial_path(tutorial3)
+
+      expect do
+        click_on 'Bookmark'
+      end.to change { UserVideo.count }.by(1)
+
+      # When I visit '/dashboard'
       visit '/dashboard'
+      # Then I should see a list of all bookmarked segments under the Bookmarked Segments section
+      # And they should be organized by which tutorial they are a part of
+      # And the videos should be ordered by their position
 
-      expect(page).to have_content("Bookmarked Segments")
-
-      within(page.first('.bookmarks-btn')) do
-        expect(page).to have_content(video1)
-        expect(page).to_not have_content(video2)
-        expect(page).to_not have_content(video3)
+      within(page.first('.bookmarked_segments')) do
+        expect(page).to have_content(video1.title)
+        expect(page).to_not have_content(video2.title)
+        expect(page).to_not have_content(video3.title)
       end
 
-      within(page.all('.bookmarks-btn')[1]) do
-        expect(page).to have_content(video2)
-        expect(page).to_not have_content(video1)
-        expect(page).to_not have_content(video3)
+      within(page.all('.bookmarked_segments')[1]) do
+        expect(page).to have_content(video2.title)
+        expect(page).to_not have_content(video1.title)
+        expect(page).to_not have_content(video3.title)
       end
 
-      within(page.all('.bookmarks-btn')[2]) do
-        expect(page).to have_content(video3)
-        expect(page).to_not have_content(video2)
-        expect(page).to_not have_content(video1)
+      within(page.all('.bookmarked_segments')[2]) do
+        expect(page).to have_content(video3.title)
+        expect(page).to_not have_content(video2.title)
+        expect(page).to_not have_content(video1.title)
       end
     #end
   end
