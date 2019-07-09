744428b87665f3455a9e2f247a65d8f95aa20356
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-09 10:59:20 -0600

    Moves test and methods to Tutorials

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -4,4 +4,12 @@ class Tutorial < ApplicationRecord
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
   scope :public_tutorials, ->{ where(classroom: false) }
+
+
+  def self.bookmarked_videos_for_user(user_id)
+    joins(:user_videos)
+      .includes(:tutorial)
+      .where(user_videos: {user_id: user_id})
+      .order(:tutorial_id, :position)
+  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -6,11 +6,4 @@ class Video < ApplicationRecord
   belongs_to :tutorial
 
   validates_presence_of :position
-
-  def self.bookmarked_videos_for_user(user_id)
-    joins(:user_videos)
-      .includes(:tutorial)
-      .where(user_videos: {user_id: user_id})
-      .order(:tutorial_id, :position)
-  end
 end
