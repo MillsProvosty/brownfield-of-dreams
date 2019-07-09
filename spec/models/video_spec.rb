ac1fe441a6013e8edff25fafc0cdc7fbe30ca948
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-09 10:19:56 -0600

    Adds bookmarked videos for user class method

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -12,4 +12,32 @@ RSpec.describe Video, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:position) }
   end
+
+  describe "class methods" do
+    it ".bookmarked_videos_for_user" do
+      user = create(:user)
+      other_user = create(:user)
+
+
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial)
+      tutorial3 = create(:tutorial)
+
+      video11 = create(:video, tutorial_id: tutorial1.id)
+      video21 = create(:video, tutorial_id: tutorial2.id)
+      video31 = create(:video, tutorial_id: tutorial3.id, position: 1)
+      video32 = create(:video, tutorial_id: tutorial3.id, position: 3)
+      video33 = create(:video, tutorial_id: tutorial3.id, position: 2)
+
+      user_video11 = create(:user_video, user: user, video: video11)
+      user_video21 = create(:user_video, user: user, video: video21)
+      user_video32 = create(:user_video, user: user, video: video32)
+      user_video33 = create(:user_video, user: user, video: video33)
+      other_user_video31 = create(:user_video, user: other_user, video: video31)
+
+      result = Video.bookmarked_videos_for_user(user.id)
+      expect(result).to eq([video11, video21, video33, video32])
+      expect(result.first.tutorial).to eq(tutorial1)
+    end
+  end
 end
