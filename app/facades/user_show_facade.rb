b717a8f6c07aae5872ae92a2be7f81cf7c073d2e
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-09 15:56:32 -0600

    Abstract out UserShowFacade#github_service

--- app/facades/user_show_facade.rb
+++ app/facades/user_show_facade.rb
@@ -7,7 +7,7 @@ class UserShowFacade
 
   def repos
     # TO DO: abstract out GithubApiService.new into private method
-    repos_hash = GithubApiService.new(@user.github_token).user_repos
+    repos_hash = github_service.user_repos
     repos_hash.map do |repo_data|
       Repo.new(repo_data)
     end.first(5)
@@ -15,14 +15,14 @@ class UserShowFacade
   end
 
   def followers
-    followers_hash = GithubApiService.new(@user.github_token).user_followers
+    followers_hash = github_service.user_followers
     followers_hash.map do |follower_data|
       GithubUser.new(follower_data)
     end
   end
 
   def followed_users
-    followed_users_hash = GithubApiService.new(@user.github_token).followed_users
+    followed_users_hash = github_service.followed_users
     followed_users_hash.map do |followed_user_data|
       GithubUser.new(followed_user_data)
     end
@@ -30,5 +30,11 @@ class UserShowFacade
 
   def friends
     @user.reload.friends
+  end
+
+  private
+
+  def github_service
+    GithubApiService.new(@user.github_token)
   end
 end
