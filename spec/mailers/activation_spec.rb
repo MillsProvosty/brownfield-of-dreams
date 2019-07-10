0843e6ab338f4280b728589510881742154f84da
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-10 17:05:47 -0600

    Passes
    Co-authored-by: Alexandra Chakeres <alexchakeres@gmail.com>

--- spec/mailers/activation_spec.rb
+++ spec/mailers/activation_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 RSpec.describe ActivationMailer, type: :mailer do
   describe "activation email after registration" do
     before :each do
-      user = create(:user)
-      @mail = ActivationMailer.register(user)
+      @user = create(:user)
+      @mail = ActivationMailer.register(@user)
     end
 
     it "renders the headers" do
       expect(@mail.subject).to eq("Activate Your Account")
-      expect(@mail.to).to eq([email])
+      expect(@mail.to).to eq([@user.email])
       expect(@mail.from).to eq(["fromTBD@example.com"])
     end
 
