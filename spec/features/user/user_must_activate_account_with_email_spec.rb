540d062592ce303461f6fe7ae528cf249778ea5c
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-07-11 13:29:24 -0600

    deletes unused files and cleans up to do blocks in test.
    Co-authored-by: Alexandra Chakeres <alexchakeres@gmail.com>

--- spec/features/user/user_must_activate_account_with_email_spec.rb
+++ spec/features/user/user_must_activate_account_with_email_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Notifications" do
+RSpec.describe "As a non-activated user" do
   include ActionMailer::TestHelper
-  describe "As a non-activated user" do
+  describe "When I click the link in my activation email" do
     before :each do
       email = 'jimbob@aol.com'
       first_name = 'Jim'
@@ -17,25 +17,21 @@ RSpec.describe "Notifications" do
       fill_in 'user[last_name]', with: last_name
       fill_in 'user[password]', with: password
       fill_in 'user[password_confirmation]', with: password
+
+      click_on 'Create Account'
+      visit activate_user_path(User.last.id)
     end
 
-    describe "when I click the link" do
-      before :each do
-        click_on 'Create Account'
-        visit activate_user_path(User.last.id)
+    it "I should be taken to an activation page" do
+      VCR.use_cassette('activation_page', record: :new_episodes) do
+        expect(page).to have_content("Thank you! Your account is now activated.")
       end
+    end
 
-      it "I should be taken to an activation page" do
-        VCR.use_cassette('activation_page', record: :new_episodes) do
-          expect(page).to have_content("Thank you! Your account is now activated.")
-        end
-      end
-
-      it "when I visit dashboard, I should see status active" do
-        VCR.use_cassette('dashboard_after_activation', record: :new_episodes) do
-          visit dashboard_path
-          expect(page).to have_content("Status: Active")
-        end
+    it "when I visit dashboard, I should see status active" do
+      VCR.use_cassette('dashboard_after_activation', record: :new_episodes) do
+        visit dashboard_path
+        expect(page).to have_content("Status: Active")
       end
     end
   end
