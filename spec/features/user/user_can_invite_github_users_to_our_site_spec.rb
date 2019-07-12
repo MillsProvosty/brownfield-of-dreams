e4891a29f0377b4bcc0b13d63409d4d962f9a156
Alexandra Chakeres <alexchakeres@gmail.com>
2019-07-12 09:12:35 -0600

    Hide invite button if current_user not authorized with github

--- spec/features/user/user_can_invite_github_users_to_our_site_spec.rb
+++ spec/features/user/user_can_invite_github_users_to_our_site_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-describe 'As a registered user on my dashboard page' do
+describe 'As a registered user (authorized with github) on my dashboard page' do
   before(:each) do
     user = create(:user_with_github)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -22,17 +22,32 @@ describe 'As a registered user on my dashboard page' do
       expect(page).to have_content('Successfully sent invite!')
     end
   end
-
+  
   it 'I cannot invite a github user with a private email' do
     VCR.use_cassette('invite_private_github_user', record: :new_episodes) do
       visit '/dashboard'
       click_link 'Send an Invite'
-
+      
       fill_in :github_handle, with: 'kylecornelissen'
       expect { click_button 'Send Invite'; sleep 1 }.to change { ActionMailer::Base.deliveries.count }.by(0)
-
+      
       expect(current_path).to eq('/dashboard')
       expect(page).to have_content("The Github user you selected doesn't have an email address associated with their account.")
+    end
+  end
+end
+
+describe 'As a registered user (not authorized with github) on my dashboard page' do
+  before(:each) do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  end
+
+  it 'I cannot invite a github user' do
+    VCR.use_cassette('no_invite_button_for_non_github_user', record: :new_episodes) do
+      visit '/dashboard'
+
+      expect(page).to_not have_link('Send an Invite')
     end
   end
 end
